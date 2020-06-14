815ae4dd40db1248567592cda76bd3e56265f4c1
Atif Naeem <atif.nm@gmail.com>
2020-06-14 17:01:48 +0500

    Practice

---
+++ app.js
@@ -0,0 +1,43 @@
+// ==== Chapter 1 Task #1
+//alert("Hello World")
+// ==== Chapter 1 Task #2 
+//alert("Error! Please enter a valid password")
+// ==== Chapter 1 Task #3
+//alert("Welcome to JS Land... \nHappy Coding!")
+// ==== Chapter 1 Task #4
+//alert("Welcome to JS Land...")
+//alert("Happy Coding!")
+// ==== Chapter 1 Task #5
+// alert("Hello.. I can run JS Through my web browser's console")
+// ==== Chapter 2 Task #1
+//var username
+// ==== Chapter 2 Task #2
+//var myName
+//myName = "Atif Naeem"
+// ==== Chapter 2 Task #3
+//var message
+//message = "Hello World"
+//alert(message)
+// ==== Chapter 2 Task #4
+//var name 
+//var age
+//var message
+//name = "Jhone Doe"
+//age = "15 years old"
+//message = "Certified Mobile Applications Development"
+//alert(name)
+//alert(age)
+//alert(message)
+// ==== Chapter 2 Task #5
+//str = "Pizza"
+//strNew = str.substring(0, str-1)
+//alert(strNew)
+
+// ==== Chapter 20 Task #7
+var items = ["cake","apple pie","cookie","chips","patties"];
+var search = prompt("Enter the vale to search");
+if(items.indexOf(search) !==-1){
+    alert("Yes " + search + " is available.")
+}else{
+    alert("No " + search + " not is available.")
+} 
