f0d868da28ddcd4c42cb952412bd7dae2326ffc6
atifnm <64380272+atifnm@users.noreply.github.com>
2020-06-18 14:09:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -32,12 +32,143 @@
 //str = "Pizza"
 //strNew = str.substring(0, str-1)
 //alert(strNew)
+// ==== Chapter 2 Task #6
+//var email
+//email = "example@gmail.com"
+//alert("My email address is " + email)
+// ==== Chapter 2 Task #7
+//var book
+//book = "A smarter way to learn JavaScript"
+//alert("I am trying to learn from the " + book)
+// ==== Chapter 2 Task #8
+//document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
+// ==== Chapter 2 Task #9
+//var design
+//design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+//alert(design)
+// ==== Chapter 3 Task #1
+//var age
+//age ="20"
+//alert("I am " + age + " years old")
+// ==== Chapter 3 Task #3
+//var birthYear
+//birthYear = "1999"
+//document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is number"
+//)
+// ==== Chapter 3 Task #4
+//var visitorName
+//var productTitle
+//var quantity
+//visitorName = "<b>John Doe</b>"
+//productTitle = "<b>T-shirt</b>"
+//quantity = "<b>5</b>"
+//document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store")
+// ==== Chapter 4 Task #1
+//var variable1 = "Hello World!",
+    //variable2 = "Testing",
+    //variable3 = 42;
+// ==== Chapter 4 Task #2
+// ======Illegal variable
+//grade_is_A+
+//F-
+//dollar$
+//76trombones
+//=======Legal Variable
+//Atif
+//atifNaeem
+//class
+//class5
+//class_name
+// ==== Chapter 4 Task #3
+//document.write("<h2>Rules for naming JS variables</h2><br><br>" + "Variable names can only contain ______, ______, ______ and ______.<br>" + "Variables must begin with a ______, ______ or _____. For example $name, _name or name <br>" + "Variable names are case _________ <br>"+"Variable names should not be JS _________")
+// ==== Chapter 5 Task #1
+//var number1
+//var number2
+//var sum
+//number1 = 3
+//number2 = 5
+//sum = number1 + number2
+//document.write("Sum of "+number1+" and "+number2+" is "+ sum )
+// ==== Chapter 5 Task #2
+//var number1
+//var number2
+//var sum
+//number1 = 3
+//number2 = 5
+//sub = number1 - number2
+//document.write("Subtraction of "+number1+" and "+number2+" is "+ sub )
+// ==== Chapter 5 Task #3
+//var value
+//var increment
+//var addition
+//var decrement
+//var remainder
+//value = 5
+//increment =value + 1
+//addition = increment + 7
+//decrement = addition - 1
+//remainder = decrement%3 
+//document.write("Value after variable declaration is defined<br>" +"Intial value: "+value+"<br> Value after increment: " + increment+"<br> Value after addition is: "+addition+"<br>Value after decrement is: "+decrement+"<br>The remainder is: "+remainder)
+// ==== Chapter 5 Task #4
+//var price 
+//var totalPrice
+//price =600
+//totalPrice = price*5
+//document.write("Total cost to buy 5 Tickets to a movie is "+totalPrice+"PKR")
+// ==== Chapter 5 Task #5
+/*var table=prompt("Enter any number ")
+var table1
+table1=table*1
+var table2
+table2=table*2
+var table3
+table3=table*3
+var table4
+table4=table*4
+var table5
+table5=table*5
+var table5
+table5=table*5
+var table6
+table6=table*6
+var table7
+table7=table*7
+var table8
+table8=table*8
+var table9
+table9=table*9
+var table10
+table10=table*10
+document.write("Table of "+table+"<br>"+table+"x1= "+table1+"<br>"+table+"x2= "+table2+"<br>"+table+"x3= "+table3+"<br>"+table+"x4= "+table4+"<br>"+table+"x5= "+table5+"<br>"+table+"x6= "+table6+"<br>"+table+"x7= "+table7+"<br>"+table+"x8= "+table8+"<br>"+table+"x9= "+table9+"<br>"+table+"x10= "+table10+"<br>")
+// ==== Chapter 5 Task #6
+var far 
+var cel 
+far=prompt("Enter temperature in Fahrenheit")
+cel=(far-32)*5/9
+document.write(far+"F is "+cel+"C")
 
-// ==== Chapter 20 Task #7
-var items = ["cake","apple pie","cookie","chips","patties"];
-var search = prompt("Enter the vale to search");
-if(items.indexOf(search) !==-1){
-    alert("Yes " + search + " is available.")
-}else{
-    alert("No " + search + " not is available.")
-} 
+// ==== Chapter 6 Task #1
+a=prompt("Enter the value of a")
+document.write("Results:<br>"+"The value of a is:"+a+"<br>")
+a=++a
+document.write("Results:<br>"+"The value of ++a is:"+a+"<br>")
+a=a++
+document.write("Results:<br>"+"The value of a++ is:"+a+"<br>")
+a=--a
+document.write("Results:<br>"+"The value of --a is:"+a+"<br>")
+a=a--
+document.write("Results:<br>"+"The value of a-- is:"+a+"<br>")
+
+// ==== Chapter 6 Task #2
+var a = 2;
+var b = 1;
+var result1
+var result2
+var result3
+var result4
+result1 = --a;
+result2 = --a - --b;
+result3 = --a - --b + ++b;
+result4 = --a - --b + ++b + b--;
+document.write("a is " + a+"<br>b is "+b+"<br>result 1 is "+result1+"<br>result 2 is "+result2+"<br>result 3 is "+result3+"<br>result 4 is "+result4)
+*/
